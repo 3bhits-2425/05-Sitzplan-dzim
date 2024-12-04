31f9a7ce2cee176e6aaadac75fd6b33ae10eeb28
mdzinic06 <mirza.dzinic06@htl-salzburg.ac.at>
2024-12-04 20:24:54 +0100

    spawning chairs on the pos1 and pos2

--- Assets/MyGame/Scripts/RoomManager.cs
+++ Assets/MyGame/Scripts/RoomManager.cs
@@ -6,6 +6,7 @@ public class RoomManager : MonoBehaviour
     [SerializeField] private StudentLayoutData[] students; //
     [SerializeField] private GameObject tablePrefab; //Prefab für Tisch
     [SerializeField] private GameObject chairPrefab; //Prefab für Stuhl
+
     void Start()
     {
         for (int row = 0; row < tableLayout.rows; row++)
@@ -14,18 +15,20 @@ public class RoomManager : MonoBehaviour
             {
                 Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
                 GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
-            }
-        }
-        
-        for (int row = 0; row < tableLayout.rows; row++ )
-        {
-            for (int col = 0; col < tableLayout.columns; col++)
-            {
-                Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0 , row * tableLayout.tableSpacing);
-                GameObject chair1 = Instantiate(chairPrefab, tablePosition, Quaternion.identity, transform);
-                
+
+                Transform pos1 = table.transform.Find("pos1");
+                Transform pos2 = table.transform.Find("pos2");
+
+                if (pos1)
+                {
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+
+                if (pos2)
+                {
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                }
             }
         }
     }
-    
 }
